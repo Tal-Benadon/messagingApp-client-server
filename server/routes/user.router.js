d66190bc13ff3409d93ebd1846c70661f2ae9ee4
Tal Ben Adon <talbenadon2@gmail.com>
2024-04-04 14:59:09 +0300

    hi

--- server/routes/user.router.js
+++ server/routes/user.router.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const userService = require('../BL/user.service')
 router.post('/register', async (req, res) => {
     try {
-
+        console.log("hi");
     } catch (error) {
 
     }
